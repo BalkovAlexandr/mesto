d6f7f22387fe51af22f30876d6ed7938eb7ea621
Alexandr Balkov <Harleta1907@yandex.ru>
2020-11-27 14:35:12 +0200

    Проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,4 @@
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-]; 
-
 const photoCardTemplate = document.querySelector('.photo-grid');
-const element = document.querySelector('.elements');
 
 const sectionProfile = document.querySelector('.profile');
 const editButton = sectionProfile.querySelector('.profile__edit-button');
@@ -38,41 +10,29 @@ const popupProfileEdit = document.querySelector('.popup_type_edit');
 const popupForm = popupProfileEdit.querySelector('.popup__form');
 const popupInputName = popupProfileEdit.querySelector('.popup__input_text_name');
 const popupInputAchievements = popupProfileEdit.querySelector('.popup__input_text_achievements');
-const popupCloseProfile = popupProfileEdit.querySelector('.popup__close-button');
 
 const popupTypeImage = document.querySelector('.popup_type_image');
 const popupImage= popupTypeImage.querySelector('.popup__image');
 const popupImageTitle = popupTypeImage.querySelector('.popup__image-title');
-const popupCloseImage = popupTypeImage.querySelector('.popup__close-button');
 
 const popupTypeAddCard = document.querySelector('.popup_type_add');
 const popupCardForm = popupTypeAddCard.querySelector('.popup__form');
 const popupCardName = popupTypeAddCard.querySelector('.popup__input_card_name');
 const popupCardLink = popupTypeAddCard.querySelector('.popup__input_card_link');
-const popupCloseCard = popupTypeAddCard.querySelector('.popup__close-button');
 
-function addPhotoCard(photoArr) {
+function addPhotoCard(photo) {
     const cardElement = document.querySelector('.photo-template').content.cloneNode(true);
     const cardImage = cardElement.querySelector('.photo-grid__image');
     const cardTitle = cardElement.querySelector('.photo-grid__title');
-    cardImage.src = photoArr.link;
-    cardTitle.textContent = photoArr.name;
-    cardElement.querySelector('.photo-grid__image').alt = photoArr.name.toLowerCase();
-
-    cardElement.querySelector('.photo-grid__delete-button').addEventListener('click', event => {
-        const gridItem = event.target.closest('.photo-grid__item');
-        gridItem.remove();
-    });
-
-    cardElement.querySelector('.photo-grid__like').addEventListener('click', event => {
-        event.target.classList.toggle('photo-grid__like_active');
-    });
+    cardImage.src = photo.link;
+    cardTitle.textContent = photo.name;
+    cardElement.querySelector('.photo-grid__image').alt = photo.name.toLowerCase();
 
     cardImage.addEventListener('click', function() {
-        showPopup(popupTypeImage);
-        popupImageTitle.textContent = photoArr.name;
+        popupImageTitle.textContent = photo.name;
         popupImage.src = cardImage.src;
         popupImage.alt = cardImage.alt;
+        showPopup(popupTypeImage);
 
     });
     return cardElement;
@@ -83,12 +43,30 @@ initialCards.forEach(function (card, newCard) {
     photoCardTemplate.append(newCard);
 });
 
+function escHandler(event) {
+    if (event.key === 'Escape') {
+    closePopup(document.querySelector('.popup_opened'));
+    }
+};
+
+function popupClickHandler(event) {
+    if (event.target.classList.contains('popup_opened')) {
+        closePopup(event.target);
+    } 
+};
+
 function showPopup(popup) {
+    popupCardForm.reset();
     popup.classList.add('popup_opened');
+    enableValidation(validationConfig);
+    document.addEventListener('keydown', escHandler);
+
 };
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', escHandler);
+
 };
 
 function popupProfile(event) {
@@ -106,29 +84,37 @@ function popupAddCard(event) {
         };
     photoCardTemplate.prepend(addPhotoCard(newCard));
     closePopup(popupTypeAddCard);
-    popupCardForm.reset();
 };
 
-popupCloseProfile.addEventListener('click', () => closePopup(popupProfileEdit));
-popupCloseImage.addEventListener('click', () => closePopup(popupTypeImage));
-popupCloseCard.addEventListener('click', function () {
-    popupCardForm.reset();
-    closePopup(popupTypeAddCard)});
+Array.from(document.querySelectorAll('.popup__close-button')).forEach(popupClose => {
+    popupClose.addEventListener('click', () => closePopup(popupClose.closest('.popup')));
+});
+
+Array.from(document.querySelectorAll('.popup')).forEach(popup => {
+    popup.addEventListener('mousedown', popupClickHandler);
+});
 
 popupForm.addEventListener('submit', popupProfile);
 popupCardForm.addEventListener('submit', popupAddCard);
 editButton.addEventListener('click', function() {
-    showPopup(popupProfileEdit);
     popupInputName.value = profileName.textContent;
     popupInputAchievements.value = profileAchievements.textContent;
-  });
+    showPopup(popupProfileEdit);
+});
 addCardButton.addEventListener('click', () => showPopup(popupTypeAddCard));
 
+photoCardTemplate.addEventListener('click', (event) => {
+    if (event.target.classList.contains('photo-grid__like')) {
+    event.target.classList.toggle('photo-grid__like_active');
+    }
+});
 
-
-
-
-
+photoCardTemplate.addEventListener('click', (event) => {
+    if (event.target.classList.contains('photo-grid__delete-button')) {
+    const gridItem = event.target.closest('.photo-grid__item');
+    gridItem.remove();
+    }
+});
 
 
 
